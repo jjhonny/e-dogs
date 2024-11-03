bed9e07c6debdac018ce3d576fd0e6aedb551168
jjhonny <ojhonny1@hotmail.com>
2024-11-02 21:11:16 -0300

    feat: initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/login";
+
 function App() {
   return (
     <>
-      <div>
-        <h1>e-dogs</h1>
-      </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
